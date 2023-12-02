9608670943d176dee4fc85ca6a6d28288707bfaa
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-12-02 11:37:38 +0530

    added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,16 +33,10 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        {
-          from_name: form.name,
-          to_name: "JavaScript Mastery",
-          from_email: form.email,
-          to_email: "hsangwan2002@gmail.com",
-          message: form.message,
-        },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        "service_him6i0p",
+        "template_y6ak664",
+        form.current,
+        "qUrHoSom2MYQhXqLg"
       )
       .then(
         () => {
